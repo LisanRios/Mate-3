719d1790365a5e9a240d42ea1da11b40ea8c91fb
LisanRios <114954842+LisanRios@users.noreply.github.com>
2024-05-03 21:18:07 -0300

    tp grafos

--- Clase 8/clase8.py
+++ Clase 8/clase8.py
@@ -1,4 +1,12 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
-cols = ['Date', 'Time', 'Latitude', 'Longitude', 'Depth', 'Magnitude Type']
-df_e = pd.read_csv(f'archs/earthquakes_1965_2016_database.csv.zip', dtype=str)[cols]
-print(df_e)
+
+url = "https://www.worldometers.info/world-population/population-by-country/"
+req = requests.get(url)
+soup = BeautifulSoup(req.text)
+data = soup.find_all("table")[0]
+df_population = pd.read_html(str(data))[0]
+df_population.head()
+export_csv = df_population.to_csv(r"archivo.csv", index = None, header = True)
